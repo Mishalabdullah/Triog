f791da60a38678cbe7f778612075d65ae6207ee6
Mishalabdullah <github.ijvob@simplelogin.co>
2023-03-19 10:41:13 +0530

    Styling

--- app/src/pages/dashboard.jsx
+++ app/src/pages/dashboard.jsx
@@ -119,21 +119,13 @@ export const Dashboard = () => {
                 }}
               ></div>
               <div className="best-post-content">
-                <div className="best-post-content-cat">
-                  December 2, 2021<span className="dot"> </span>Blog
-                </div>
-                <div className="best-post-content-title">
-                  Lorem ipsum dolor sit amet, consectetur
-                </div>
+                <div className="best-post-content-title">Welcome To Triog</div>
                 <div className="best-post-content-sub">
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
-                  do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                  Ut enim ad minim veniam, quis nostrud exercitation ullamco
-                  laboris nisi ut aliquip ex ea commodo consequat. Duis aute
-                  irure dolor in reprehenderit in voluptate velit esse cillum
-                  dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-                  cupidatat non proident, sunt in culpa qui officia deserunt
-                  mollit anim id est laborum.
+                  Triog is a web3 bloging platform Lorem ipsum dolor sit amet
+                  consectetur adipisicing elit. Rem atque repellendus
+                  voluptatibus eius sapiente ipsa reiciendis molestiae nihil
+                  dicta dolorem commodi quis nobis esse minus temporibus sequi,
+                  id earum quibusdam.
                 </div>
               </div>
             </article>
